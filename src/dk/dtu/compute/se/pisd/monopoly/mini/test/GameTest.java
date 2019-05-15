722d2b410b6da7a0a05ad8f5cfb544fb30f25254
Siff <siff.ravn@gmail.com>
2019-05-15 16:30:33 +0200

    GameTest created

---
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/test/GameTest.java
@@ -0,0 +1,20 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void drawCardFromDeck() {
+    }
+
+    @Test
+    void returnCardToDeck() {
+    }
+
+    @Test
+    void shuffleCardDeck() {
+    }
+}

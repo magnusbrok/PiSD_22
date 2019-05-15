735e561cafaa10b93a2df2c2314efad14f36f9c4
Siff <siff.ravn@gmail.com>
2019-05-15 14:10:04 +0200

    GameControllerTest JUnit5 class added

---
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/test/GameControllerTest.java
@@ -0,0 +1,51 @@
+package dk.dtu.compute.se.pisd.monopoly.mini.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import dk.dtu.compute.se.pisd.monopoly.mini.controller.GameController;
+
+class GameControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void makeMove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void houseOffer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void moveToSpace() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void takeChanceCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnChanceCardToDeck() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void obtainCash() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void payment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paymentFromBank() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paymentToBank() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void playerBrokeTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void playerBrokeToBank() {
+    }
+}
